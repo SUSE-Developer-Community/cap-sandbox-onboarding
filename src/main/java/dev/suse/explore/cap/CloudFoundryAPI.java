48978dee4394441651c22185ad475f6800d81c5e
Tim Irnich <tim.irnich@suse.com>
2020-02-12 21:24:50 +0100

    WIP - DO NOT MERGE Adding space and spaceRole creation

--- src/main/java/dev/suse/explore/cap/CloudFoundryAPI.java
+++ src/main/java/dev/suse/explore/cap/CloudFoundryAPI.java
@@ -4,6 +4,9 @@ import org.cloudfoundry.operations.useradmin.CreateUserRequest;
 import org.cloudfoundry.operations.organizations.CreateOrganizationRequest;
 import org.cloudfoundry.operations.spaces.CreateSpaceRequest;
 import org.cloudfoundry.operations.useradmin.SetOrganizationRoleRequest;
+import org.cloudfoundry.operations.useradmin.SetSpaceRoleRequest;
+import org.cloudfoundry.operations.useradmin.SpaceRole;
+import org.cloudfoundry.operations.useradmin.OrganizationRole;
 
 import org.cloudfoundry.operations.CloudFoundryOperations;
 
@@ -45,32 +48,61 @@ public class CloudFoundryAPI {
 		CloudFoundryOperations ops = context.getBean(CloudFoundryOperations.class);
 		
 		//**********create the org***************
-		//create org name from email: foo.bar@bar.com -> foor_bar_bar_com
-		String orgname = email.replace("\w", "_");
+		//create org name from email: foo.bar@bar.com -> foo_bar_bar_com
+		String orgname = email.replace("^\\w", "_");
 
-		//How do I tell which parameters the request needs?
-		//TODO: replace quota name string with an env variable or something
+		// TODO: replace quota name string with an env variable or something
 		CreateOrganizationRequest req = CreateOrganizationRequest.builder().organizationName(orgname).quotaDefinitionName("sandbox").build();
 		//Do I need to call .block() in the end? Why if so?
 		ops.organizations().create(req).block();
 
-		//**********make user org manager and developer*************
-		SetOrganizationRoleRequest req = SetOrganizationRoleRequest.builder().organizationName(orgname).organizationRole("Manager").build();
-		ops.useradmin().setOrganizationRole(req).block();
-		SetOrganizationRoleRequest req = SetOrganizationRoleRequest.builder().organizationName(orgname).organizationRole("Developer").build();
-		ops.useradmin().setOrganizationRole(req).block();
+		//**********make user org manager*************
+		ops.userAdmin().setOrganizationRole(
+				SetOrganizationRoleRequest.builder().organizationName(orgname).organizationRole(OrganizationRole.MANAGER).username(email).build()
+				).block();
 
 		//***********create the default spaces************
-		//TODO: there is probably an elegant way to fold these into one call somehow
-		//TODO: Does the user get roles assigned for these spaces automatically?
-		CreateSpaceRequest req = CreateSpaceRequest.builder().name("dev").organization(orgname).build();
-		ops.spaces().create(req).block();
-		CreateSpaceRequest req = CreateSpaceRequest.builder().name("test").organization(orgname).build();
-		ops.spaces().create(req).block();
-		CreateSpaceRequest req = CreateSpaceRequest.builder().name("prod").organization(orgname).build();
-		ops.spaces().create(req).block();
-		CreateSpaceRequest req = CreateSpaceRequest.builder().name("samples").organization(orgname).build();
-		ops.spaces().create(req).block();
+		// TODO: there is probably an elegant way to fold these into one call somehow
+		// TODO: Does the user get roles assigned for these spaces automatically?
+		ops.spaces().create(
+				CreateSpaceRequest.builder().name("dev").organization(orgname).build()
+				).block();
+		ops.userAdmin().setSpaceRole(
+				SetSpaceRoleRequest.builder().organizationName(orgname).spaceName("dev").spaceRole(SpaceRole.MANAGER).username(email).build()
+				).block();
+		ops.userAdmin().setSpaceRole(
+				SetSpaceRoleRequest.builder().organizationName(orgname).spaceName("dev").spaceRole(SpaceRole.DEVELOPER).username(email).build()
+				).block();
+
+		ops.spaces().create(
+				CreateSpaceRequest.builder().name("test").organization(orgname).build()
+				).block();
+		ops.userAdmin().setSpaceRole(
+				SetSpaceRoleRequest.builder().organizationName(orgname).spaceName("test").spaceRole(SpaceRole.MANAGER).username(email).build()
+				).block();
+		ops.userAdmin().setSpaceRole(
+				SetSpaceRoleRequest.builder().organizationName(orgname).spaceName("test").spaceRole(SpaceRole.DEVELOPER).username(email).build()
+				).block();
+
+		ops.spaces().create(
+				CreateSpaceRequest.builder().name("prod").organization(orgname).build()
+				).block();
+		ops.userAdmin().setSpaceRole(
+				SetSpaceRoleRequest.builder().organizationName(orgname).spaceName("prod").spaceRole(SpaceRole.MANAGER).username(email).build()
+				).block();
+		ops.userAdmin().setSpaceRole(
+				SetSpaceRoleRequest.builder().organizationName(orgname).spaceName("prod").spaceRole(SpaceRole.DEVELOPER).username(email).build()
+				).block();
+		
+		ops.spaces().create(
+				CreateSpaceRequest.builder().name("samples").organization(orgname).build()
+				).block();
+		ops.userAdmin().setSpaceRole(
+				SetSpaceRoleRequest.builder().organizationName(orgname).spaceName("samples").spaceRole(SpaceRole.MANAGER).username(email).build()
+				).block();
+		ops.userAdmin().setSpaceRole(
+				SetSpaceRoleRequest.builder().organizationName(orgname).spaceName("samples").spaceRole(SpaceRole.DEVELOPER).username(email).build()
+				).block();
 
 
 		//PushApplicationRequest req = PushApplicationRequest.builder().application()
