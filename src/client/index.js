553a79aba1b031dcd977ea2339505ad5ac11a5fa
Andrew Gracey <andrew.gracey@suse.com>
2020-08-14 08:34:19 -0700

    refactoring with services instead of env

--- src/client/index.js
+++ src/client/index.js
@@ -3,17 +3,28 @@ import UAA from './uaa.js'
 import CfHttp from './http_client.js'
 import UaaHttp from './http_client_uaa.js'
 
+
+const getService = (type, name)=>(
+  JSON.parse(process.env['VCAP_SERVICES'])[type]
+    .find((service)=>(service.name==name))
+)
+
+const cf_api = getService('user-provided', 'cf_api').credentials
+const uaa_api = getService('user-provided', 'uaa_api').credentials
+
+
+
 const http_client = new CfHttp(
-  'https://' + process.env.CF_API,
-  'https://' + process.env.UAA_API,
-  process.env.CF_ADMIN_USER,
-  process.env.CF_ADMIN_PASS)
+  'https://' + cf_api.uri,
+  'https://' + uaa_api.uri,
+  cf_api.user,
+  cf_api.password)
 
 const http_client_uaa = new UaaHttp(
-  'https://' + process.env.CF_API,
-  'https://' + process.env.UAA_API,
-  process.env.UAA_ADMIN_CLIENT_ID,
-  process.env.UAA_ADMIN_SECRET)
+  'https://' + cf_api.uri,
+  'https://' + uaa_api.uri,
+  uaa_api.client_id,
+  uaa_api.client_secret)
 
 export const cf = new CF(http_client)
 export const uaa = new UAA(http_client_uaa)
