eaa920845cbd2293261a98d8108ebbec87d299df
Andrew Gracey <andrew.gracey@suse.com>
2020-10-28 09:39:34 -0700

    Fixing user list to actually work

--- src/pull_user_list.js
+++ src/pull_user_list.js
@@ -1,9 +1,13 @@
 import {uaa} from './client/index.js'
 
+// to run this, use `cf ssh onboarding-js -c 'cd app; ./node dist/pull_user_list.js'` as an admin with the right permissions
+
 setTimeout(()=>{
-  console.log('Starting pull')
-  uaa.findUsers([]).then((users)=>{
-    console.log(users[2])
+  console.error('Starting pull')
+  uaa.findUsers([]).then(lis=>{
+    lis.filter((u=>(u.emails.length>0))).map((u)=>(u.emails[0].value)).forEach(email => {
+      console.log(email)
+    })
   })
 
 },5000)
